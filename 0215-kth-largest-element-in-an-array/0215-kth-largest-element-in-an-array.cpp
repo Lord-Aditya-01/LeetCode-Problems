757a6240dd9c951c068089d3db54d08c93a5befc
Aditya Girhe <adityagirhe83@gmail.com>
2026-06-22 11:36:55 +0530

    Time: 36 ms (44.75%), Space: 72.6 MB (13.12%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,36 +1,14 @@
 class Solution {
 public:
-
-    void heapifyMax(vector<int>& nums, int n, int i) {
-        int largest = i;      
-        int left = 2 * i + 1;
-        int right = 2 * i + 2;
-
-        if (left < n && nums[left] > nums[largest])
-            largest = left;
-
-        if (right < n && nums[right] > nums[largest])
-            largest = right;
-
-        if (largest != i) {
-            swap(nums[i], nums[largest]);
-            heapifyMax(nums, n, largest);
-        }
-    }
-
-    void heapSortAscending(vector<int>& nums, int n) {
-        // Build Max Heap
-        for (int i = n / 2 - 1; i >= 0; i--)
-            heapifyMax(nums, n, i);
-
-        // Extract elements
-        for (int i = n - 1; i > 0; i--) {
-            swap(nums[0], nums[i]);
-            heapifyMax(nums, i, 0);
-        }
-    }
     int findKthLargest(vector<int>& nums, int k) {
-        heapSortAscending(nums, nums.size());
-        return nums[nums.size() - k]; 
+        priority_queue<int, vector<int> , greater<int>> pq;
+
+        for(int i : nums) {
+            pq.push(i);
+            if(pq.size() > k) {
+                pq.pop();
+            }
+        }
+        return pq.top();
     }
 };
